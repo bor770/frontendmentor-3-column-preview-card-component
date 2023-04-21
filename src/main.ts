a1767deaa5ad3797386e4154b7ddd0c4d115f201
Baruch Giverts <bor770@gmail.com>
2023-04-21 16:06:05 +0300

    initial setup with material

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
-import { AppModule } from './app/app.module';
+import { AppComponent } from './app/app.component';
 
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, { providers: [provideAnimations()] });
